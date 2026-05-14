73c8f16fd2f520761eabbe61fdd2094f6d5a2241
Finnete <finnetekawira@gmail.com>
2026-05-14 15:18:35 -0400

    final frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,8 @@
-import { useState } from "react";
-import axios from "axios";
-
-function App() {
-  const [message, setMessage] = useState("");
-  const [result, setResult] = useState("");
-
-  const analyzeMessage = async () => {
-    try {
-      const res = await axios.post("http://127.0.0.1:8000/analyze", {
-        message: message,
-      });
-
-      setResult(res.data.result);
-    } catch (err) {
-      setResult("Error connecting to backend");
-    }
-  };
-
+export default function App() {
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>Phishing Detection Tool</h1>
-
-      <textarea
-        rows="6"
-        cols="60"
-        placeholder="Paste email or message here..."
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-
-      <br />
-
-      <button onClick={analyzeMessage} style={{ marginTop: "10px" }}>
-        Analyze
-      </button>
-
-      <h2>Result:</h2>
-      <pre>{result}</pre>
+    <div>
+      <h1>Welcome!</h1>
+      <h2>This is Finnete's Phishing Detection Tool!</h2>
     </div>
   );
 }
-
-export default App;
